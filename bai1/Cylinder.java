527a2660f0869d0bb5e90e91ecf34aeb26f24f80
ThyLuu <luuthythy2003@gmail.com>
2023-03-05 13:47:22 +0700

    5/3

---
+++ bai1/Cylinder.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    public double getVolume(){
+        return getArea() * height;
+    }
+}
